543ee33643580e1e2ed409885db480367448dd60
KaanOzdogan <necdetkaanoz@hotmail.com>
2022-10-10 03:28:02 +0300

    Add random color mode

--- script.js
+++ script.js
@@ -1,70 +1,102 @@
+const CANVAS_PIXEL = 600;
+const DEFAULT_SIZE = 16;
+const DEFAULT_PEN_COLOR = "black";
+const DEFAULT_MODE = "mouseenter";
+let currentSize = DEFAULT_SIZE;
+let currentPenColor = DEFAULT_PEN_COLOR;
+let currentMode = DEFAULT_MODE;
+let randomColorMode = false;
+
 const canvas = document.querySelector(".canvas");
-const clearBtn = document.querySelector(".clear-btn");
-const clickMode = document.querySelector(".click-mode");
-const defaultBtn = document.querySelector(".default-btn");
+canvas.style.height = `${CANVAS_PIXEL}px`;
+canvas.style.width = `${CANVAS_PIXEL}px`;
+
 const sizeSlider = document.querySelector(".size-slider");
 const sliderText = document.querySelector(".slider-text");
 
-console.log(sizeSlider);
-
-const DEFAULT_SIZE = 16;
-const DEFAULT_COLOR = "white";
-const DEFAULT_MODE = "mouseenter";
-let currentSize = DEFAULT_SIZE;
-let currentMode = DEFAULT_MODE;
+const randomColors = document.querySelector(".random-colors");
+const clearBtn = document.querySelector(".clear-btn");
+const clickMode = document.querySelector(".click-mode");
+const defaultBtn = document.querySelector(".default-btn");
 
 sizeSlider.min = 1;
 sizeSlider.max = 64;
 sizeSlider.onchange = () => {
-  drawCanvas(sizeSlider.value, DEFAULT_COLOR, currentMode);
+  drawCanvas(sizeSlider.value, currentMode);
 };
 
-const deleteSquares = () => {
-  const squares = document.querySelectorAll(".pixel");
-  for (let i = 0; i < squares.length; i++) {
-    squares[i].remove();
+const deletePixels = () => {
+  const pixels = document.querySelectorAll(".pixel");
+  for (let i = 0; i < pixels.length; i++) {
+    pixels[i].remove();
   }
 };
 
-const drawCanvas = (size, color, mode) => {
-  deleteSquares();
+const getRandomColor = () => {
+  const c1 = Math.floor(Math.random() * 256);
+  const c2 = Math.floor(Math.random() * 256);
+  const c3 = Math.floor(Math.random() * 256);
+
+  return `rgb(${c1}, ${c2}, ${c3})`;
+};
+
+const drawCanvas = (size, mode) => {
+  deletePixels();
   currentSize = size;
   sizeSlider.value = size;
   sliderText.innerHTML = `${size} x ${size}`;
   currentMode = mode;
 
   let numOfPixel = size * size;
-  let squareWidth = 500 / size;
+  let pixelWidth = CANVAS_PIXEL / size;
 
   for (let i = 0; i < numOfPixel; i++) {
-    let square = document.createElement("div");
-    square.className = "pixel";
-    square.style.backgroundColor = color;
-    square.style.border = "1px solid black";
-    square.addEventListener(mode, () => {
-      square.style.backgroundColor = "black";
+    let pixel = document.createElement("div");
+    pixel.className = "pixel";
+    pixel.style.backgroundColor = "white";
+    pixel.style.border = "1px solid black";
+    pixel.addEventListener(mode, () => {
+      if (randomColorMode == true) {
+        currentPenColor = getRandomColor();
+      } else {
+        currentPenColor = DEFAULT_PEN_COLOR;
+      }
+      pixel.style.backgroundColor = currentPenColor;
     });
-    canvas.appendChild(square);
+    canvas.appendChild(pixel);
   }
 
   canvas.style.display = "grid";
-  canvas.style.gridTemplateColumns = `repeat(${size}, ${squareWidth}px)`;
+  canvas.style.gridTemplateColumns = `repeat(${size}, ${pixelWidth}px)`;
 };
 
-drawCanvas(DEFAULT_SIZE, DEFAULT_COLOR, DEFAULT_MODE);
+drawCanvas(DEFAULT_SIZE, DEFAULT_MODE);
+
+randomColors.addEventListener("click", () => {
+  randomColorMode = !randomColorMode;
+  if (randomColorMode) {
+    document
+      .querySelector(":root")
+      .style.setProperty("--randomColors-zIndex", 0);
+  } else {
+    document
+      .querySelector(":root")
+      .style.setProperty("--randomColors-zIndex", -1);
+  }
+});
 
 clickMode.addEventListener("click", () => {
   if (currentMode == "click") {
     return;
   }
 
-  drawCanvas(currentSize, DEFAULT_COLOR, "click");
+  drawCanvas(currentSize, "click");
 });
 
 clearBtn.addEventListener("click", () => {
-  drawCanvas(currentSize, DEFAULT_COLOR, currentMode);
+  drawCanvas(currentSize, currentMode);
 });
 
 defaultBtn.addEventListener("click", () => {
-  drawCanvas(DEFAULT_SIZE, DEFAULT_COLOR, DEFAULT_MODE);
+  drawCanvas(DEFAULT_SIZE, DEFAULT_MODE);
 });
